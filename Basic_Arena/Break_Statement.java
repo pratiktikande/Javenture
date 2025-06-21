4d548ae00c947d5489436117d3dfe25b7275f51b
unknown <pratiktikande12@gmail.com>
2025-06-21 21:58:22 +0530

    Basic_Arena

---
+++ Basic_Arena/Break_Statement.java
@@ -0,0 +1,11 @@
+package Basic_Arena;
+
+public class Break_Statement {
+    public static void main(String[] args) {
+        int a= 10;
+        for(int i=1; i<=a; i++){
+            System.out.println("Executed");
+            // break;
+        }
+    }
+}
---
+++ Basic_Arena/Do_While_Loop.java
@@ -0,0 +1,13 @@
+package Basic_Arena;
+
+public class Do_While_Loop {
+    public static void main(String[] args) {
+        int a=2;
+        do {
+            a++;
+            System.out.println("Value Updated");
+        } while (a<=10);{
+            System.out.println("While Loop area");
+        }
+    }
+}
