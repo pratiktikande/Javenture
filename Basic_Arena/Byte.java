a8d0fdee13ae502b46c11b2d3589108c2ee1a305
unknown <pratiktikande12@gmail.com>
2025-06-22 23:36:26 +0530

    Basic_Arena

---
+++ Basic_Arena/Byte.java
@@ -0,0 +1,11 @@
+package Basic_Arena;
+
+public class Byte {
+    public static void main(String[] args) {
+        byte b1=20;
+        byte b2=36;
+
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
