4ceba2151d3f6f79dfe55713623a490d905438ed
unknown <pratiktikande12@gmail.com>
2025-06-28 23:37:40 +0530

    Basic_Arena

--- Basic_Arena/Reverse_Number.java
+++ Basic_Arena/Reverse_Number.java
@@ -3,11 +3,19 @@ package Basic_Arena;
 import java.util.Scanner;
 
 public class Reverse_Number {
-    Scanner sc = new Scanner(System.in);
-    int reverse=0;
-    int Num= sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int reverse = 0;
+        System.out.println("Enter the Number to Reverse");
+        int Num = sc.nextInt();
 
-    
-    
-    
+        while (Num != 0) {
+            int lastdigit = Num % 10;
+            reverse = reverse * 10 + lastdigit; 
+            Num = Num / 10;
+        }
+        sc.close();
+        System.out.println(" Reverse Number");
+        System.out.println(reverse);
+    }
 }
--- Basic_Arena/Swap_Without_Temp.java
+++ Basic_Arena/Swap_Without_Temp.java
@@ -2,12 +2,18 @@ package Basic_Arena;
 
 public class Swap_Without_Temp {
     public static void main(String[] args) {
-        int a=15;
-        int b=20;
-        System.out.println("Number Before Swap" );
+        int a = 15;
+        int b = 20;
+        System.out.println("Number Before Swap");
         System.out.println(a);
         System.out.println(b);
 
-        
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After Swap");
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
